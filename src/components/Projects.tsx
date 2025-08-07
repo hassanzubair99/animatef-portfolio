9fce49310db8f9754127ec228047bf77c00c98ca
hassan zubair <hassanzubair4243@gmail.com>
2025-08-07 21:33:18 +0000

    make project like 3 project in one line

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -52,7 +52,7 @@ export function Projects() {
             Here are some of the projects I've worked on. Each one was a unique challenge and a learning experience.
           </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 lg:gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-12">
           {projects.map((project, index) => (
             <ProjectCard key={index} {...project} />
           ))}
